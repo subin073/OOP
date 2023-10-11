11630f6d5e8e01eedea646c96414bb17d6aef812
sumin <sumint730@naver.com>
2023-10-11 20:50:08 +0900

    first commit

---
+++ src/Practics/Homework3.java
@@ -0,0 +1,34 @@
+package Practics;
+import java.util.Scanner;
+
+public class Homework3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("몇 개의 수를 입력할 예정인가요?:  ");
+        int NumCount = sc.nextInt();
+
+        int[] NumArr = new int[NumCount]; //배열만들고
+
+        System.out.println("수를 입력하세요: ");
+        //int N = sc.nextInt();
+
+
+        int max = NumArr[0];
+        int min = NumArr[0];
+
+        for (int i=0; i< NumArr.length; i++){
+            NumArr[i] = sc.nextInt();
+
+            if (NumArr[i] >= max){
+                System.out.printf("최댓값: %d", NumArr[i]);
+            }
+            if (NumArr[i] <= min){
+                System.out.printf("최솟값: %d", min);
+            }
+        }
+
+
+
+    }
+}
