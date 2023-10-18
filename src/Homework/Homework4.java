843698966e5d8a19448504a6b926533eb00c9519
sumin <sumint730@naver.com>
2023-10-19 00:07:36 +0900

    first commit

--- src/Homework/Homework4.java
+++ src/Homework/Homework4.java
@@ -1,13 +1,29 @@
 package Homework;
 import java.util.Scanner;
 
-
-
-
-
 public class Homework4 {
-    public static void main(String[] args) {
 
+    int gcd(int m, int n) {
+
+        if (n == 0) {
+            return m;
+        }
+        if (m % n == 0) {
+            return n;
+        }
+        return gcd(n, m % n);
     }
+
+            public static void main(String[] args) {
+
+            Homework4 h = new Homework4();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("두 정수를 입력: ");
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+
+            System.out.printf("최대공약수: %d", h.gcd(m, n));
+    }
+
 
 }
